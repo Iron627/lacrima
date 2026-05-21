cff18cf1d77785e94e20b24b1a414a4b67eecd49
Iron6271 <irononyt69@gmail.com>
2026-05-21 22:32:30 +0530

    Elo difference: 167.4 +/- 35.4, pesto tables

--- eval.go
+++ eval.go
@@ -1,33 +1,56 @@
 package lacrima
 
 func Eval(pos *Position, colour int8) int {
-	score := 0
-	for _, piece := range pos.Board {
-		switch piece {
-		case WhitePawn:
-			score += 100
-		case WhiteKnight:
-			score += 320
-		case WhiteBishop:
-			score += 330
-		case WhiteRook:
-			score += 500
-		case WhiteQueen:
-			score += 900
-		case BlackPawn:
-			score -= 100
-		case BlackKnight:
-			score -= 320
-		case BlackBishop:
-			score -= 330
-		case BlackRook:
-			score -= 500
-		case BlackQueen:
-			score -= 900
+	mg := [2]int{}
+	eg := [2]int{}
+	gamePhase := 0
+
+	for sq, piece := range pos.Board {
+		if IsOffBoard(sq) || piece == Empty {
+			continue
 		}
+
+		side := 0
+		if piece < 0 {
+			side = 1
+		}
+
+		pt := int(PieceType(piece)) - 1
+
+		pstSq := sq64(sq)
+
+		if side == 1 {
+			pstSq ^= 56
+		}
+
+		mg[side] += MgValue[pt] + MgPestoTable[pt][pstSq]
+		eg[side] += EgValue[pt] + EgPestoTable[pt][pstSq]
+
+		gamePhase += GamePhaseInc[pt]
 	}
+
+	if gamePhase > 24 {
+		gamePhase = 24
+	}
+
+	mgScore := mg[0] - mg[1]
+	egScore := eg[0] - eg[1]
+
+	egPhase := 24 - gamePhase
+
+	score := (mgScore*gamePhase +
+		egScore*egPhase) / 24
+
 	if colour < 0 {
 		score = -score
 	}
+
 	return score
+}
+
+func sq64(sq int) int {
+	rank := sq >> 4
+	file := sq & 7
+
+	return (7-rank)*8 + file
 }
