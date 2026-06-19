9208c559cf69b3b88c71230bd808e54b6765f012
Iron6271 <irononyt69@gmail.com>
2026-06-19 21:41:21 +0530

    blah blah blah
    BENCH: 7826466

--- lacrima.go
+++ lacrima.go
@@ -151,7 +151,7 @@ func RunUCIWithIO(input io.Reader, output io.Writer, errOutput io.Writer) {
 		switch fields[0] {
 
 		case "uci":
-			writeLine("id name Lacrima v1.0.9.8")
+			writeLine("id name Lacrima v1.0.9.9")
 			writeLine("id author Iron")
 			writeLine("option name Hash type spin default", defaultHashMB, "min", minHashMB, "max", maxHashMB)
 			writeLine("option name Threads type spin default 1 min 1 max 1")
